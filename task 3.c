452be1d5ba69719c03964d90bf0dd2220935507f
Munroemuigai <Codermunroe24@gmail.com>
2025-11-19 00:25:17 +0300

    Add files via upload

---
+++ task 3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<string.h>
+
+int main(){
+    char str[20];
+    int length;
+
+    printf("munroe\n");
+    length=strlen(str);
+    printf("The length of the string is:%d\n",length);
+    printf("%19s\n",str);
+    scanf("%s\n",str);
+return 0;
+}
